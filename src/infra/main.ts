ecaa8ccb69c149a28a5949125c8e00f70d3bdd96
Alexandre Moura de Souza <alemoura23@hotmail.com>
2025-09-26 14:56:04 -0300

    fix corls vercel

--- src/infra/main.ts
+++ src/infra/main.ts
@@ -5,18 +5,29 @@ import { EnvService } from './env/env.service'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   const configService = app.get(EnvService)
+  const port = configService.get('PORT')
 
-  const port = configService.get('PORT') || 3000
-  // const origin = configService.get('FRONTEND_URL') || 'http://localhost:5173'
-
+  // Habilita CORS de forma dinâmica
   app.enableCors({
-    origin: true,
+    origin: (incomingOrigin, callback) => {
+      // Lista de domínios permitidos
+      const allowedOrigins = [
+        'https://teste-orcamento-front-end-vq9n.vercel.app',
+        'https://seu-dominio-principal.vercel.app',
+      ]
+
+      // Se não tiver origin (por exemplo, requisições do Postman) ou estiver na lista, permite
+      if (!incomingOrigin || allowedOrigins.includes(incomingOrigin)) {
+        callback(null, true)
+      } else {
+        callback(new Error(`Origin ${incomingOrigin} not allowed by CORS`))
+      }
+    },
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   })
 
+  console.log(`App listening on port ${port}`)
   await app.listen(port)
-  console.log(`App listening on port ${port} with frontend ${origin}`)
 }
 bootstrap()
-
